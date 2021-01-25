3461641f402d613eb886b5337ded230d847b9fc3
Sidney Antommarchi <sidna4@yahoo.com>
2021-01-25 07:34:37 -0500

    Lesson 178

---
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * User: sidna
+ * Date: 1/24/2021 10:04 AM
+ */
+@Configuration
+public class JmsConfig {
+
+    public static final String MY_QUEUE = "my-hello-world";
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgjms.listener;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+/**
+ * User: sidna
+ * Date: 1/24/2021 1:04 PM
+ */
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) {
+        System.out.println("I got a message!!!");
+
+        System.out.println(helloWorldMessage);
+
+        // throw new RuntimeException("foo");
+    }
+}
